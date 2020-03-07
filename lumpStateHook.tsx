3175aa542e76f1b37334bc7a2496d14c97a4d26e
Andrew Beasley <alloydbeasley@gmail.com>
2020-03-07 09:38:31 -0600

    removes count from hook

--- lumpStateHook.tsx
+++ lumpStateHook.tsx
@@ -3,14 +3,12 @@ import { useState } from "react";
 
 export const useLumpState = (state: any) => {
     let copy = { ...state };
-    let count = 0;
+    
     for (let i in copy) {
         const [get, set] = useState(copy[i])
 
-        copy[i] = { get, set };
-        count++;
+        copy[i] = { get, set };        
     }
-
-    // if (count == Object.keys(copy).length)
-        return copy;
+    
+    return copy;
 }
